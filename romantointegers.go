911043c017204b616a0464bbe978826345df1b5f
Efe Kaya <sithban2@gmail.com>
2024-03-10 17:45:26 -0600

    Update romantointegers.go

--- romantointegers.go
+++ romantointegers.go
@@ -1,36 +1,24 @@
 package main
 
-import "fmt"
-
 func romanToInt(s string) int {
-
-	romanCharacters := map[string]int{
-		"I": 1,
-		"V": 5,
-		"X": 10,
-		"L": 50,
-		"C": 100,
-		"D": 500,
-		"M": 1000,
+	romanMap := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
 	}
-
-	total := 0
-
-	for i := 0; i < len(s); i++ {
-		if i < len(s)-1 && romanCharacters[string(s[i])] < romanCharacters[string(s[i+1])] {
-			total -= romanCharacters[string(s[i])]
+	
+	result := 0
+	for i := (len(s) - 1); i >= 0; i-- {
+		if (i == (len(s) - 1)) || (romanMap[s[i]] >= romanMap[s[i+1]]) {
+			result += romanMap[s[i]]
 		} else {
-			total += romanCharacters[string(s[i])]
+			result -= romanMap[s[i]]
 		}
 	}
 
-	return total
-}
-
-func main() {
-	fmt.Println(romanToInt("III"))
-	fmt.Println(romanToInt("IV"))
-	fmt.Println(romanToInt("IX"))
-	fmt.Println(romanToInt("LVIII"))
-	fmt.Println(romanToInt("MCMXCIV"))
+	return result
 }
